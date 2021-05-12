98c76a2d782e30ad5179b0dec73b5da477708783
mzhao15 <cliffish0408@gmail.com>
2021-05-11 22:34:34 -0400

    renamed file names

--- algorithms/Array/leetcode41_firstMissingPositive.py
+++ algorithms/Array/leetcode41_firstMissingPositive.py
@@ -1,16 +1,12 @@
+def segredate(nums):
+    # move all positive nums to the left and all the
+    i, j = 0, len(nums)-1
+
 
 def firstMissingPositive(nums):
 
-	segredate(nums)
+    segredate(nums)
 
 
-def segredate(nums):
-	# move all positive nums to the left and all the 
-	i,j=0,len(nums)-1
-	while i<j:
-		
-
-
-
-nums = [ 2, 3, -7, 6, 8, 1, -10, 15]
+nums = [2, 3, -7, 6, 8, 1, -10, 15]
 print(firstMissingPositive(nums))
