7706c57c2c16bb812a44433c18e5aeee63cb4424
mzhao15 <cliffish0408@gmail.com>
2021-06-12 21:05:42 -0400

    random update

--- algorithms/Search/binary_search.py
+++ algorithms/Search/binary_search.py
@@ -1,23 +1,16 @@
+def binary_search(nums, target):
 
-
-def binary_search(arr,x):
-	"""find x in a sorted array """
-	right = len(arr)-1
-	left = 0
-	while left <= right:
-		mid = (left + right)//2
-		if arr[mid] == x:
-			return mid
-		elif arr[mid] < x:
-			left = mid + 1
-		else:
-			right = mid - 1
-	return -1
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        mid = l + (r-l)//2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return -1
 
 
 a = [2, 3, 4, 10, 40, 50]
-print(binary_search(a,3))
-
-
-
-		
+print(binary_search(a, 3))
